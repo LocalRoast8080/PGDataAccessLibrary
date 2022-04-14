4591d2f8059990a7f280c321f195f745b3d5513c
Chris <localroast8080@gmail.com>
2022-04-14 13:47:17 -0500

    Got data from azure logged into console ui

---
+++ PGDataAccessLibrary/SqlCrud.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PGDataAccessLibrary
+{
+    public class SqlCrud
+    {
+        DataAccess db = new DataAccess();
+        public List<PersonModel> GetAllPeople(string connectionString)
+        {
+            string sql = "SELECT * FROM public.\"People\" " ;
+            var output = db.LoadData<PersonModel, dynamic>(sql, new { }, connectionString).ToList();
+            return output;
+        }
+    }
+}
---
+++ PGDataAccessUI/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+
+
+using Microsoft.Extensions.Configuration;
+using PGDataAccessLibrary;
+
+var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+     .AddJsonFile("appSettings.json");
+var config = builder.Build();
+
+
+SqlCrud dataAccess = new SqlCrud();
+string conn = config.GetConnectionString("PGRES");
+
+var connString = "Host=dbserver1.postgres.database.azure.com;Username=serverthing15;Password=7ayB9WE32#f6;Database=test";
+
+List<PersonModel> customers = dataAccess.GetAllPeople(connString);
+foreach (var person in customers)
+{
+    Console.WriteLine($"{ person.FirstName},:");
+}
+
+
+
+
+Console.ReadLine();
